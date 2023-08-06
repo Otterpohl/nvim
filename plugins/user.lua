098d4de4bdb3330ac919b14ce4a1edea9bca425e
Tristan Otterpohl <tristan.otterpohl@outlook.com>
2023-08-06 20:46:44 +0100

    initial config

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,35 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODO's in Telescope" },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    keys = {
+      { "<leader>x", desc = "Trouble" },
+      { "<leader>X", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics" },
+      { "<leader>x", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics" },
+      { "<leader>q", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List" },
+    },
+    opts = {
+      use_diagnostic_signs = true,
+      action_keys = {
+        close = { "q", "<esc>" },
+        cancel = "<c-e>",
+      },
+    },
+  },
+  {
+    "mbbill/undotree",
+    keys = {
+      { "<leader>U", "<cmd>UndotreeToggle<cr>", desc = "Toggle undotree" },
+    },
+  },
 }
